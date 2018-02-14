e820eb2bde06326c7e96223cf35f8869344f2b66
Kuvakin Sergey <kuvakin@gmail.com>
2018-02-14 23:40:32 +0300

    :walking:

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"path/filepath"
 
 	api "github.com/lafin/vk"
 )
+
+func newfileUploadRequest(from, to string) (*http.Request, error) {
+	src, err := http.Get(from)
+	if err != nil {
+		return nil, err
+	}
+	defer src.Body.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(from))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = io.Copy(part, src.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", to, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, err
+}
 
 func main() {
 	clientID := os.Getenv("CLIENT_ID")
@@ -19,5 +55,41 @@ func main() {
 		return
 	}
 
-	log.Println("done")
+	server, err := api.GetUploadServer(117456732)
+	if err != nil {
+		log.Fatalf("[main:api.GetUploadServer] error: %s", err)
+		return
+	}
+
+	request, err := newfileUploadRequest("https://pp.userapi.com/c639631/v639631968/14282/yS0K3aa6zEM.jpg", server.Response.UploadURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		fmt.Println(string(body))
+		var fileUploadRequest api.ResponseFileUploadRequest
+		if err := json.Unmarshal(body, &fileUploadRequest); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		fmt.Println(fileUploadRequest.Photo, fileUploadRequest.Hash)
+		result, err := api.SavePhoto(fileUploadRequest.Server, fileUploadRequest.Photo, fileUploadRequest.Hash)
+		if err != nil {
+			log.Fatalf("[main:api.SavePhoto] error: %s", err)
+			return
+		}
+		fmt.Println(result)
+	}
 }
